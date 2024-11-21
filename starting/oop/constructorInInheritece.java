70d133cba3e25bcd5e128a43e119715366204752
mdriyadtalukder <riyadtalukder67@gmail.com>
2024-11-21 22:04:57 +0600

    updated

--- starting/oop/constructorInInheritece.java
+++ starting/oop/constructorInInheritece.java
@@ -11,6 +11,8 @@ class Base {
 
 class PBase extends Base {
     PBase() {
+        // String temp=super.define_me() super.functionName dile uporer Base class er ta
+        // ashbe same name er..r emni dile ei class er function ta ashbe same name er.
         super(45); // eta na dile 1st base constructor print hot..eivbei krte hy sob
         System.out.println("Hello Pbase");
     }
